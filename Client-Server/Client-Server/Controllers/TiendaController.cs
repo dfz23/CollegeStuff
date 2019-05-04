7adc7c6e201d7202e9877125dacf6626abb4c160
Diego Felipe Jaramillo <dief.jara@hotmail.com>
2019-05-03 21:50:10 -0500

    intial commit

---
+++ Client-Server/Client-Server/Controllers/TiendaController.cs
@@ -0,0 +1,97 @@
+﻿using Client_Server.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client_Server.Controllers
+{
+    [Authorize]
+    public class TiendaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult Get()
+        {
+            var objeto = new Entities();
+            var articulos = objeto.Articuloes.ToList().OrderBy(p => p.ReferenciaArticulo);
+            return Json(articulos, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Update(Articulo Objetoupdate)
+        {
+            List<Articulo> oDataNueva = null;
+            var objeto = new Entities();
+            var data = System.Web.HttpContext.Current.Request["models"];
+            if (!string.IsNullOrEmpty(data))
+            {
+                oDataNueva = (new System.Web.Script.Serialization.JavaScriptSerializer()).Deserialize<List<Articulo>>(data);
+                if (oDataNueva != null && oDataNueva.Count > 0)
+                {
+
+                    oDataNueva.All(x =>
+                    {
+                        var A = (from a in objeto.Articuloes
+                                 where a.ReferenciaArticulo == x.ReferenciaArticulo
+                                 select a).FirstOrDefault();
+                        A.NombreArticulo = x.NombreArticulo;
+                        A.CantidadArticulo = x.CantidadArticulo;
+                        A.ValorArticulo = x.ValorArticulo;
+                        return true;
+                    });
+
+                }
+            }
+
+            objeto.SaveChanges();
+            return Json(oDataNueva, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Create()
+        {
+            List<Articulo> oDataNueva = null;
+            var objeto = new Entities();
+            var data = System.Web.HttpContext.Current.Request["models"];
+            if (!string.IsNullOrEmpty(data))
+            {
+                oDataNueva = (new System.Web.Script.Serialization.JavaScriptSerializer()).Deserialize<List<Articulo>>(data);
+                if (oDataNueva != null && oDataNueva.Count > 0)
+                {
+                    objeto.Articuloes.AddRange(oDataNueva);
+                }
+            }
+
+            objeto.SaveChanges();
+            return Json(oDataNueva, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Destroy()
+        {
+            List<Articulo> oDataNueva = null;
+            var objeto = new Entities();
+            var data = System.Web.HttpContext.Current.Request["models"];
+            if (!string.IsNullOrEmpty(data))
+            {
+                oDataNueva = (new System.Web.Script.Serialization.JavaScriptSerializer()).Deserialize<List<Articulo>>(data);
+                if (oDataNueva != null && oDataNueva.Count > 0)
+                {
+
+                    oDataNueva.All(x =>
+                    {
+                        var A = (from a in objeto.Articuloes
+                                 where a.ReferenciaArticulo == x.ReferenciaArticulo
+                                 select a).FirstOrDefault();
+                        objeto.Articuloes.Remove(A);
+                        return true;
+                    });
+
+                }
+            }
+            objeto.SaveChanges();
+            return Json(oDataNueva, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Client-Server/Client-Server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Client_Server.Startup))]
+namespace Client_Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
